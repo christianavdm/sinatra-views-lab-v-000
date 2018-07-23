1d0da51c98d1849a1bd4724a551563a0b93555a6
Christiana Vandermale <christianavdm@gmail.com>
2018-07-23 13:11:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,17 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
-
+	
+	get '/hello' do
+		erb :hello.erb
+	end
+	
+	get '/goodbye' do
+		erb :goodbye.erb
+	end
+	
+	get '/date' do
+		erb :date.erb
+	end
 
 end
